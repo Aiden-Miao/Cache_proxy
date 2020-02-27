4cbc1535040ca9a18725bb54b1403ffe965685ee
kefanlin <kefanlin@kflin-mbp15.local>
2020-02-27 14:49:51 -0500

    most updated...

--- handler.cpp
+++ handler.cpp
@@ -65,11 +65,7 @@ string Handler::receiveHeader(int fd){
   vector<char> header(1, 0);
   int index = 0;
   int nbytes;
-  while (true) {
-    nbytes = recv(fd, &header.data()[index], 1 ,MSG_WAITALL);
-    if(nbytes == -1){
-      continue;
-    }
+  while ((nbytes = recv(fd, &header.data()[index], 1 ,MSG_WAITALL)) > 0) {
     if (header.size() > 4) {
       if (header.back() == '\n' && header[header.size() - 2] == '\r' &&
           header[header.size() - 3] == '\n' &&
@@ -89,13 +85,8 @@ string Handler::receiveContent(int fd,int content_length){
   vector<char> content(1,0);
   int index = 0;
   int nbytes = 0;
-
-  while (true){
-    nbytes = recv(fd, &content.data()[index], 1 ,MSG_WAITALL);
-    if(nbytes == -1){
-      continue;
-    }
-    content.resize(content.size()+1);
+  while ((nbytes = recv(fd, &content.data()[index], 1 ,MSG_WAITALL)) > 0){
+    //content.resize(content.size()+1);
     index += nbytes;
     content_length -= nbytes;
     if(content_length==0){
